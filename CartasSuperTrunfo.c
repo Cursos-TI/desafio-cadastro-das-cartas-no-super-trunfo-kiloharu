ec2a8790158795474419b063b18cb3f66c8848a6
Daniel Mascarenhas <60680884+kiloharu@users.noreply.github.com>
2025-02-21 00:40:40 +0000

    Commit antes do teste final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,21 +10,25 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     char codEstado1;
+    int carta1 = 0;
+    int carta2 = 0;
     int codCidade1;
     char nomeCidade1[256];
     char codCarta1;
-    float populacao1;
+    unsigned long int populacao1;
     float area1;
     float pib1;
     int numpontoturistico1;
+    float superpoder1;
     char codEstado2;
     int codCidade2;
     char nomeCidade2[256];
     char codCarta2;
-    float populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int numpontoturistico2;
+    float superpoder2;
     float densidadepop1;
     float pibpercapta1;
     float densidadepop2;
@@ -34,6 +38,7 @@ int main() {
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("***********Cadastro das Carta do Super Trungo***********\n\n");
     printf("Cadastro da Carta 1\n");
     printf("Insira o Código do Estado (Letras entre A a H):\n");
     scanf("%c", &codEstado1);
@@ -78,7 +83,11 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("\n\nCarta Cadastrada com Sucesso!\n\n");
+
+
+    printf("\n\nCarta Cadastrada com Sucesso!\n\n\n");
+
+    //Exibição dos itens da primeira carta
     printf("Estado: %c.\n", codEstado1);
     printf("Código: %c%d.\n", codEstado1, codCidade1);
     printf("Nome da Cidade: %s.\n", nomeCidade1);
@@ -89,8 +98,11 @@ int main() {
     densidadepop1 = populacao1/area1;
     printf("Densidade Populacional: %.2f.\n", densidadepop1);
     pibpercapta1 = pib1/populacao1;
-    printf("PIB per Capta: %.2f.\n", pibpercapta1);
+    printf("PIB per Capta: %.2f.\n\n\n", pibpercapta1);
+    superpoder1 = populacao1 + area1 + pib1 + numpontoturistico1 + pibpercapta1 + (1/densidadepop1);
+    printf("O 'Super Poder' dessa carta é de %.2f\n\n", superpoder1);
 
+    //Exibição dos itens da segunda carta
     printf("Estado: %c.\n", codEstado2);
     printf("Código: %c%d.\n", codEstado2, codCidade2);
     printf("Nome da Cidade: %s.\n", nomeCidade2);
@@ -101,7 +113,83 @@ int main() {
     densidadepop2 = populacao2/area2;
     printf("Densidade Populacional: %.2f.\n", densidadepop2);
     pibpercapta2 = pib2/populacao2;
-    printf("PIB per Capta: %.2f.\n", pibpercapta2);
+    printf("PIB per Capta: %.2f.\n\n", pibpercapta2);
+    superpoder2 = populacao2 + area2 + pib2 + numpontoturistico2 + pibpercapta2 + (1/densidadepop2);
+    printf("O 'Super Poder' dessa carta é de %.2f\n\n", superpoder2);
+
+
+    //Comparações para definir o vencedor. As variáveis carta1 e carta 2 são os placares, de acordo com cada vitória
+    //Comparação POPULAÇÃO
+    if (populacao1>populacao2) {
+        printf("Carta %c%d venceu o confronto entre POPULAÇÕES.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Carta %c%d venceu o confronto entre POPULAÇÕES.\n", codEstado2, codCidade2);
+        carta2++;
+    }
+
+    //Comparação ÁREA
+    if (area1>area2) {
+        printf("Carta %c%d venceu o confronto entre suas ÁREAS.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Carta %c%d venceu o confronto entre suas ÁREAS.\n", codEstado2, codCidade2);
+        carta2++;
+    }
+
+    //Comparação PIB
+    if (pib1>pib2) {
+        printf("Carta %c%d venceu o confronto entre o valor do seu PIB.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Carta %c%d venceu o confronto entre o valor do seu PIB.\n", codEstado2, codCidade2);
+        carta2++;
+    }
+
+    //Comparação NÚMERO DE PONTOS TURÍSTICOS
+    if (numpontoturistico1>numpontoturistico2) {
+        printf("Carta %c%d venceu o confronto entre a Quantidade de PONTOS TURÍSTICOS.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Carta %c%d venceu o confronto entre a Quantidade de PONTOS TURÍSTICOS.\n", codEstado2, codCidade2);
+        carta2++;
+    }
+
+    //Comparação DENSIDADE POPULACIONAL
+    if (populacao1<populacao2) {
+        printf("Carta %c%d venceu o confronto entre DENSIDADE POPULACIONAL.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Carta %c%d venceu o confronto entre DENSIDADE POPULACIONAL.\n", codEstado2, codCidade2);
+        carta2++;
+    }
+
+    //Comparação PIB PER CAPTA
+    if (pibpercapta1>pibpercapta2) {
+        printf("Carta %c%d venceu o confronto entre PIB PER CAPTA.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Carta %c%d venceu o confronto entre PIB PER CAPTA.\n", codEstado2, codCidade2);
+        carta2++;
+    }
+
+    printf("\n\nPor enquanto o PLACAR GERAL é carta %c%d %d x %d %c%d. Vamos para o último embate!", codEstado1, codCidade1, carta1, carta2, codEstado2, codCidade2);
+
+    //Comparação SUPER PODER
+    if (superpoder1>superpoder2) {
+        printf("Pela união de seus poderes, a carta %c%d venceu o confronto entre seus SUPER PODERES.\n", codEstado1, codCidade1);
+        carta1++;
+    }else {
+        printf("Pela união de seus poderes, a carta %c%d venceu o confronto entre seus SUPER PODERES.\n", codEstado2, codCidade2);
+        carta2++;
+    }
     
-    return 0;
+    //Comparação final, declarando o vencedor geral.
+    if (carta1>carta2) {
+        printf("E O VENCEDOR DO EMBATE É A CARTA %c%d, COM UM PLACAR DE %c%d %d x %d %c%d. PARABÉNS PARA O POVO DE %s.\n", codEstado1, codCidade1, codEstado1, codCidade1, carta1, carta2, codEstado2, codCidade2, nomeCidade1);
+    }else {
+        printf("E O VENCEDOR DO EMBATE É A CARTA %c%d, COM UM PLACAR DE %c%d %d x %d %c%d. PARABÉNS PARA O POVO DE %s.\n", codEstado2, codCidade2, codEstado1, codCidade1, carta1, carta2, codEstado2, codCidade2, nomeCidade2);
+    }
+
+        return 0;
 }
